d8b388b4dc7209412167c339f5d32f99dece81b4
Bill Khk <bill.khk@gmail.com>
2025-03-20 12:56:18 +1100

    Function get_GPU_data OK

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ comp_dict = {
 # worksheet.insert_image(cell_location, image_path)
 
 # Get info
-CPU_URL2 = 'https://ledenicheur.fr/c/processeurs'
 CPU_URL = 'https://pcpartpicker.com/products/cpu/'
+GPU_URL = 'https://pcpartpicker.com/products/video-card/'
 
 
 # Function to get all the raw data from a URL, return a BS4 object
@@ -125,29 +125,32 @@ def get_raw_data(URL, computer_obj):
     time.sleep(5)  # Allow time for JavaScript to load
 
     print('-----Filtering')
-    # Filter website results with XLS configuration
-    filter_btn = driver.find_element(By.XPATH,
-                                     '//*[@id="products"]/div[4]/div[1]/div[2]/section/div/div[1]/div/div[1]/a[1]')
-    filter_btn.click()
-    for selector, conf in [("C", "core"), ("th", "thread"), ("A", "clock_speed")]:
-        clickable = False
-        div_filter = "#filter_slide_"
-        left_filter = ".obj-filter-slide-left"
-        while not clickable:
-            try:
-                web_conf_clickable = driver.find_element(By.CSS_SELECTOR, f'{div_filter}{selector} {left_filter} a')
-                web_conf_clickable.click()
-                clickable = True
-            except ElementClickInterceptedException:
-                print('Scrolling')
-                driver.execute_script("document.querySelector('.offCanvas__content').scrollBy(0, 500);")
-        web_conf_input = driver.find_element(By.CSS_SELECTOR, f'{div_filter}{selector} {left_filter} input')
-        web_conf_input.send_keys(getattr(computer_obj.cpu, conf))  # To render dynamic
+    filtering = False
+    if filtering:
+        # Filter website results with XLS configuration
+        filter_btn = driver.find_element(By.XPATH,
+                                         '//*[@id="products"]/div[4]/div[1]/div[2]/section/div/div[1]/div/div[1]/a[1]')
+        filter_btn.click()
+        for selector, conf in [("C", "core"), ("th", "thread"), ("A", "clock_speed")]:
+            clickable = False
+            div_filter = "#filter_slide_"
+            left_filter = ".obj-filter-slide-left"
+            while not clickable:
+                try:
+                    web_conf_clickable = driver.find_element(By.CSS_SELECTOR, f'{div_filter}{selector} {left_filter} a')
+                    web_conf_clickable.click()
+                    clickable = True
+                except ElementClickInterceptedException:
+                    print('Scrolling')
+                    driver.execute_script("document.querySelector('.offCanvas__content').scrollBy(0, 500);")
+            web_conf_input = driver.find_element(By.CSS_SELECTOR, f'{div_filter}{selector} {left_filter} input')
+            web_conf_input.send_keys(getattr(computer_obj.cpu, conf))  # To render dynamic
 
-    web_hide_filter = driver.find_element(By.XPATH,
-                                          '//*[@id="products"]/div[4]/div[1]/div[2]/section/div/div[1]/div/div[2]/header/nav/div/div/a')
-    web_hide_filter.click()
-    driver.get(driver.current_url)  # Getting filtered URL
+        web_hide_filter = driver.find_element(By.XPATH,
+                                              '//*[@id="products"]/div[4]/div[1]/div[2]/section/div/div[1]/div/div[2]/header/nav/div/div/a')
+        web_hide_filter.click()
+        driver.get(driver.current_url)  # Getting filtered URL
+
     # Parse the page
     soup = BeautifulSoup(driver.page_source, "html.parser")
     # Find all rows in the CPU table
@@ -158,30 +161,35 @@ def get_raw_data(URL, computer_obj):
 
 
 # Extract the information of CPUs from the webpage
-# TODO add socket, Boost speed, thread and link
+# TODO merge the two get data
 def get_CPU_data(computer_obj):
     print('----------Getting online data for CPU----------')
-    cpu_rows = get_raw_data(CPU_URL, computer_obj)
+    data_rows = get_raw_data(CPU_URL, computer_obj)
     cpu_name = [str(data)[int(str(data).find('<div class="td__nameWrapper"> <p>') + 33):str(data).find(
-        '</p> <div class="td__rating"')] for data in cpu_rows]
-    # cpu_core = [int(str(data)[int(str(data).find('Core Count</h6>') + 15):str(data).find('Core Count</h6>') + 16]) for
-    #             data in cpu_rows]
-
+        '</p> <div class="td__rating"')] for data in data_rows]
     cpu_core = [int(str(data)[int(str(data).find('Core Count</h6>') + 15):str(data).find(
         '</td> <td class="td__spec td__spec--2">')]) for
-                data in cpu_rows]
-
+                data in data_rows]
     cpu_perf = [float(str(data)[int(str(data).find('Performance Core Clock</h6>') + 27):str(data).find(
-        'GHz</td> <td class="td__spec td__spec--3')]) for data in cpu_rows]
-    cpu_price = [str(data)[int(str(data).find('<td class="td__price">') + 23):str(data).find(
-        '<button class="td__add button button--small')] for data in cpu_rows]
+        'GHz</td> <td class="td__spec td__spec--3')]) for data in data_rows]
+    cpu_price = [str(data)[int(str(data).find('<td class="td__price">$') + 23):str(data).find(
+        '<button class="td__add button button--small')] for data in data_rows]
     return [cpu_name, cpu_core, cpu_perf, cpu_price]
 
 
-# TODO
 def get_GPU_data(computer_obj):
-    pass
-
+    print('----------Getting online data for GPU----------')
+    data_rows = get_raw_data(GPU_URL, computer_obj)
+    gpu_name = [str(data)[int(str(data).find('<div class="td__nameWrapper"> <p>') + 33):str(data).find(
+        '</p> <div class="td__rating"')] for data in data_rows]
+    gpu_ram = [int(str(data)[int(str(data).find('Memory</h6>') + 11):str(data).find(
+        ' GB</td> <td class="td__spec td__spec--3">')]) for
+                data in data_rows]
+    gpu_perf = [int(str(data)[int(str(data).find('Core Clock</h6>') + 15):str(data).find(
+        ' MHz</td> <td class="td__spec td__spec--4')]) for data in data_rows]
+    gpu_price = [str(data)[int(str(data).find('<td class="td__price">') + 23):str(data).find(
+        '<button class="td__add button button--small')] for data in data_rows]
+    return [gpu_name, gpu_ram, gpu_perf, gpu_price]
 
 # Get the min configuration from the file
 def get_min_xls_conf():  #
@@ -227,8 +235,6 @@ def get_min_xls_conf():  #
                         for key, value in min_para:
                             if key == attr:
                                 setattr(getattr(min_conf, pc_comp), attr, value)
-
-
             else:
                 print(f'Cant find {comp} in dict element')
                 return None
@@ -244,7 +250,8 @@ def get_min_xls_conf():  #
 # Create a computer object and put XLS info
 min_conf = get_min_xls_conf()
 # Use the created computer to filter online info
-cpu_info = get_CPU_data(min_conf)
-print(f'Retrieved data from URL :\n{cpu_info}')
+#cpu_info = get_CPU_data(min_conf)
+gpu_info = get_GPU_data(min_conf)
+print(f'Retrieved data from URL :\n{gpu_info}')
 
 #workbook.close()
